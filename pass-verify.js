d4746f45f1fdd6c13dd332d255248ca8c4694959
liwgar <wilenri341@hotmail.com>
2024-03-26 20:27:37 -0500

    hashing para contraseñas con bcryptjs.

--- pass-verify.js
+++ pass-verify.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 
 async function verifyPassword() {
   const myPassword = 'admin12345';
-  const hash = '$2b$10$SbdsmdnzN2YXjzD2rdfhn.RKi/n0iEXU7mJlzSB2HNSQE.sGYikXm';
+  const hash = '$2b$10$tD4B50/FmUiN2qpw2bgjNuENpFAZh5qsUpATd8sAmyUhz4ZCdhogK';
   const isMatch = await bcrypt.compare(myPassword, hash); 
   console.log(isMatch);
 };
